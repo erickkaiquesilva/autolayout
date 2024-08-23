c63da2f1120b2657cac27ee6a6715a8cb1c76c43
Erick Kaique Da Silva <erick.ksilva@luizalabs.com>
2024-08-23 00:09:30 -0300

    Initial Commit

---
+++ AutoLayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Magalu on 23/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
